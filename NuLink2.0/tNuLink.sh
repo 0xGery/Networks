b8d222c66ddf99fb2728ace4d2af4afb12cd7c21
Scryotic <mrswagery@gmail.com>
2024-01-02 17:44:47 +0700

    Update tNuLink.sh
    try again

--- NuLink2.0/tNuLink.sh
+++ NuLink2.0/tNuLink.sh
@@ -14,6 +14,16 @@ echo "    \:\__\        \::/  /       \::/  /         \:\__\      \::/  /       
 echo "     \/__/         \/__/         \/__/           \/__/       \/__/         \/__/         \/__/    "
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 
+# System update and upgrade
+sudo dpkg --configure -a
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get -f install
+
+# Installing essential packages
+sudo apt install -y python3-pip ca-certificates curl gnupg expect
+pip install virtualenv
+
 # Function to prompt for password input with hidden input
 prompt_for_password() {
     local password
@@ -39,66 +49,32 @@ echo "Enter worker account password (min 8 characters):"
 NULINK_OPERATOR_ETH_PASSWORD=$(prompt_for_password "")
 
 # Installing and setting up Geth
-sleep 2
 GETH_URL="https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.10.23-d901d853.tar.gz"
 GETH_DIR="/root/geth-linux-amd64-1.10.23-d901d853"
-wget -qO- $GETH_URL | tar xvz -C /root || { echo "Failed to download and extract Geth"; exit 1; }
-cd $GETH_DIR || { echo "Failed to navigate to Geth directory"; exit 1; }
+wget -qO- $GETH_URL | tar xvz -C /root
+cd $GETH_DIR
 
-# Prompting user for Geth account password manually and creating a new account
+# Creating a new account
 ./geth account new --keystore ./keystore
 
-# User input for public address
-echo "Enter the public address of the key (with 0x prefix): "
-read PUBLIC_ADDRESS
+# Prompting user to enter the public address manually
+read -p "Enter the public address of the key (with 0x prefix): " PUBLIC_ADDRESS
 
-# Docker Installation and setup
-sleep 2
+# Docker installation
+sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # Verifying Docker installation
-docker --version || { echo "Failed to verify Docker installation"; exit 1; }
+docker --version || { echo "Failed to install Docker"; exit 1; }
 
-# Asking user for funding confirmation
-ask_for_funding_confirmation() {
-    while true; do
-        read -p "Please fund your $PUBLIC_ADDRESS with testBNB, have you funded it? (Y/N): " response
-        case $response in
-            [Yy]* ) break;;
-            [Nn]* ) echo "Waiting for funding. Please fund your account.";;
-            * ) echo "Please answer Y (yes) or N (no).";;
-        esac
-    done
-}
+# Docker commands with stored passwords
+docker run -it --rm -p 9151:9151 -v $PWD/keystore:/root/keystore -v $NULINK_DIR:/code -v $NULINK_DIR:/home/circleci/.local/share/nulink -e NULINK_KEYSTORE_PASSWORD=$NULINK_KEYSTORE_PASSWORD nulink/nulink nulink ursula init --signer keystore:///root/keystore/$KEYSTORE_FILE --eth-provider https://data-seed-prebsc-2-s2.binance.org:8545 --network horus --payment-provider https://data-seed-prebsc-2-s2.binance.org:8545 --payment-network bsc_testnet --operator-address $PUBLIC_ADDRESS --max-gas-price 10000000000 || { echo "Failed to run Docker container for initialization"; exit 1; }
 
-ask_for_funding_confirmation
-
-# Docker run commands
-sleep 2
-docker run -it --rm \
--p 9151:9151 \
--v $PWD/keystore:/root/keystore \
--v $NULINK_DIR:/code \
--v $NULINK_DIR:/home/circleci/.local/share/nulink \
--e NULINK_KEYSTORE_PASSWORD=$NULINK_KEYSTORE_PASSWORD \
-nulink/nulink nulink ursula init \
---signer "keystore:///root/keystore/$KEYSTORE_FILE" \
---eth-provider https://data-seed-prebsc-2-s2.binance.org:8545 \
---network horus \
---payment-provider https://data-seed-prebsc-2-s2.binance.org:8545 \
---payment-network bsc_testnet \
---operator-address $PUBLIC_ADDRESS \
---max-gas-price 10000000000 || { echo "Failed to run Docker container for initialization"; exit 1; }
-
-docker run --restart on-failure -d \
---name ursula \
--p 9151:9151 \
--v $PWD/keystore:/root/keystore \
--v $NULINK_DIR:/code \
--v $NULINK_DIR:/home/circleci/.local/share/nulink \
--e NULINK_KEYSTORE_PASSWORD=$NULINK_KEYSTORE_PASSWORD \
--e NULINK_OPERATOR_ETH_PASSWORD=$NULINK_OPERATOR_ETH_PASSWORD \
-nulink/nulink nulink ursula run --no-block-until-ready || { echo "Failed to run Docker container 'ursula'"; exit 1; }
+docker run --restart on-failure -d --name ursula -p 9151:9151 -v $PWD/keystore:/root/keystore -v $NULINK_DIR:/code -v $NULINK_DIR:/home/circleci/.local/share/nulink -e NULINK_KEYSTORE_PASSWORD=$NULINK_KEYSTORE_PASSWORD -e NULINK_OPERATOR_ETH_PASSWORD=$NULINK_OPERATOR_ETH_PASSWORD nulink/nulink nulink ursula run --no-block-until-ready || { echo "Failed to run Docker container 'ursula'"; exit 1; }
 
 docker logs -f ursula
